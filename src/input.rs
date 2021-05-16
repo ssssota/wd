c7a71618a319050dfe1a4ba696d7ed716d19a698
Sotaro Tommykawa <tommysouta@gmail.com>
2021-05-16 12:51:12 +0900

    add tests

--- src/input.rs
+++ src/input.rs
@@ -38,19 +38,131 @@ pub fn read_stdin() -> Result<String, String> {
     }
 }
 
-pub fn split_words(line: &str, delimiter: Option<&str>) -> Vec<String> {
+fn split_words(line: &str, delimiter: Option<&str>) -> Vec<String> {
     match delimiter {
         Some(d) => line.split(d).map(|w| w.to_string()).collect(),
         None => line.split_whitespace().map(|w| w.to_string()).collect(),
     }
 }
 
-pub fn parse_input(lines: &str, delimiter: Option<&str>) -> Vec<Vec<String>> {
-    lines
+pub fn parse_input(input: &str, delimiter: Option<&str>) -> Vec<Vec<String>> {
+    input
         .split("\n")
         .map(str::trim)
         .map(|v| split_words(v, delimiter))
         .map(|v| v.into_iter().map(|w| w.trim().to_string()))
         .map(Iterator::collect)
         .collect()
+}
+
+#[test]
+fn test_split_words() {
+    struct TestCase {
+        line: &'static str,
+        delimiter: Option<&'static str>,
+        expected: Vec<String>,
+    }
+    let testcases = vec![
+        TestCase {
+            line: "1 2 3 a b c",
+            delimiter: None,
+            expected: vec![
+                "1".to_string(),
+                "2".to_string(),
+                "3".to_string(),
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+            ],
+        },
+        TestCase {
+            line: "abc def ghi jkl",
+            delimiter: None,
+            expected: vec![
+                "abc".to_string(),
+                "def".to_string(),
+                "ghi".to_string(),
+                "jkl".to_string(),
+            ],
+        },
+        TestCase {
+            line: "||hi|",
+            delimiter: Some("|"),
+            expected: vec![
+                "".to_string(),
+                "".to_string(),
+                "hi".to_string(),
+                "".to_string(),
+            ],
+        },
+    ];
+
+    for testcase in testcases {
+        assert_eq!(
+            split_words(testcase.line, testcase.delimiter),
+            testcase.expected
+        );
+    }
+}
+
+#[test]
+fn test_parse_input() {
+    struct TestCase {
+        input: &'static str,
+        delimiter: Option<&'static str>,
+        expected: Vec<Vec<String>>,
+    }
+
+    let testcases = vec![
+        TestCase {
+            input: "a b c",
+            delimiter: None,
+            expected: vec![vec!["a".to_string(), "b".to_string(), "c".to_string()]],
+        },
+        TestCase {
+            input: "abc def\nghi jkl",
+            delimiter: None,
+            expected: vec![
+                vec!["abc".to_string(), "def".to_string()],
+                vec!["ghi".to_string(), "jkl".to_string()],
+            ],
+        },
+        TestCase {
+            input: "Name,Age,Height,Weight\nTaro,5,180,30\nJiro,3,150,50\nSaburo,1,90,3",
+            delimiter: Some(","),
+            expected: vec![
+                vec![
+                    "Name".to_string(),
+                    "Age".to_string(),
+                    "Height".to_string(),
+                    "Weight".to_string(),
+                ],
+                vec![
+                    "Taro".to_string(),
+                    "5".to_string(),
+                    "180".to_string(),
+                    "30".to_string(),
+                ],
+                vec![
+                    "Jiro".to_string(),
+                    "3".to_string(),
+                    "150".to_string(),
+                    "50".to_string(),
+                ],
+                vec![
+                    "Saburo".to_string(),
+                    "1".to_string(),
+                    "90".to_string(),
+                    "3".to_string(),
+                ],
+            ],
+        },
+    ];
+
+    for testcase in testcases {
+        assert_eq!(
+            parse_input(testcase.input, testcase.delimiter),
+            testcase.expected
+        );
+    }
 }
--- src/wd.rs
+++ src/wd.rs
@@ -5,3 +5,48 @@ pub fn wd(words: Vec<String>, number: Vec<u64>) -> Vec<String> {
         .filter_map(|(i, word)| number.contains(&(i as u64 + 1)).then(|| word.clone()))
         .collect()
 }
+
+#[test]
+fn test_wd() {
+    struct TestCase {
+        words: Vec<String>,
+        number: Vec<u64>,
+        expected: Vec<String>,
+    }
+    let testcases = vec![
+        TestCase {
+            words: vec!["a".to_string()],
+            number: vec![1],
+            expected: vec!["a".to_string()],
+        },
+        TestCase {
+            words: vec!["a".to_string(), "b".to_string()],
+            number: vec![1],
+            expected: vec!["a".to_string()],
+        },
+        TestCase {
+            words: vec!["abc".to_string(), "def".to_string()],
+            number: vec![2],
+            expected: vec!["def".to_string()],
+        },
+        TestCase {
+            words: vec!["a".to_string(), "b".to_string()],
+            number: vec![1, 2],
+            expected: vec!["a".to_string(), "b".to_string()],
+        },
+        TestCase {
+            words: vec!["a".to_string(), "b".to_string(), "c".to_string()],
+            number: vec![1, 3],
+            expected: vec!["a".to_string(), "c".to_string()],
+        },
+        TestCase {
+            words: vec!["a".to_string(), "b".to_string()],
+            number: vec![0, 3],
+            expected: vec![],
+        },
+    ];
+
+    for testcase in testcases {
+        assert_eq!(wd(testcase.words, testcase.number), testcase.expected);
+    }
+}
